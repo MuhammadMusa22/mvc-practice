052b2e0cb5e03f1f2336336b8d2ff09e873a30c2
Muhammad Musa <42924055+MuhammadMusaaa@users.noreply.github.com>
2022-05-06 15:17:49 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MVC Practice',
+      title: 'Github Action',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
