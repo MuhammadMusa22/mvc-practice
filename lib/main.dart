53254364f9a79ec73ef788c02f84d1a4d087a9f9
Muhammad Musa <42924055+MuhammadMusaaa@users.noreply.github.com>
2022-05-10 13:09:33 +0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+// - uses: actions/upload-artifact@v1
+// with:
+// name: release-apk
+// path: build/app/outputs/flutter-apk/app-release.apk
+// upload:
+// runs-on: ubuntu-latest
+// name: Upload APK
+// steps:
+// - name: Upload to slack step
+// - uses: adrey/slack-file-upload-action@master
+// with:
+// token: ${{ secrets.SLACK_TOKEN }}
+// path: build/app/outputs/flutter-apk/app-release.apk
+// channel: random
+
+// steps:
+// - name: Post to a Slack channel
+// id: slack
+// uses: slackapi/slack-github-action@v1.18.0
+// with:
+// channel-id: 'C032NCAKJPR'
+// slack-message: "Does it Work"
+// env:
+// SLACK_BOT_TOKEN: ${{ secrets.SLACK_BOT_TOKEN }}
